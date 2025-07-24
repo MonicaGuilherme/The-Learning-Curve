5a2199bca195a7036ad189a0729598f60ee033b9
MonicaGuilherme <mfguilherme@gmail.com>
2025-07-24 22:39:47 +0100

    first try at something. In need of more research and a few more tries

---
+++ Android_Two-screen-app/app/src/main/java/com/example/twoscreenapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.twoscreenapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String NAME_TEXT = "name_text";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText name = findViewById(R.id.text_name);
+        Button previewBtn = findViewById((R.id.button_preview));
+        previewBtn.setOnClickListener(v -> {
+            String nameText = name.getText().toString();
+
+            Intent intent = new Intent(getApplicationContext(), PreviewActivity.class);
+            intent.putExtra(NAME_TEXT,nameText);
+
+            startActivity(intent);
+        });
+
+
+    }
+}
