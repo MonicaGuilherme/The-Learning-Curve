5a079e907be08a270b7c6c805baa39288e09bba9
MonicaGuilherme <mfguilherme@gmail.com>
2025-07-28 23:05:48 +0100

    day four ex: inicial configuration

---
+++ Android Training/Day4/app/src/main/java/com/training/usermanager/UserDatabaseHelper.java
@@ -0,0 +1,46 @@
+package com.training.usermanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "user_manager.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_USERS = "users";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_AGE = "age";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_AVATAR = "avatar_url";
+
+    public UserDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_USERS + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_NAME + " TEXT," +
+                COLUMN_AGE + " INTEGER," +
+                COLUMN_EMAIL + " TEXT," +
+                COLUMN_AVATAR + " TEXT)";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+
+
+
+
+
+    
+    // methods to do later: add user, update user, delete user and getAllUsers
+}
